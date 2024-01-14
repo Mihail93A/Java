5a15ce551370011fcb8217753536b1a1b8aa5fb8
Mihail93A <strannodan@mail.ru>
2024-01-14 16:06:46 +0500

    1

---
+++ src/main/java/guru/qa/data/Card.java
@@ -0,0 +1,10 @@
+package guru.qa.data;
+
+public class Card {
+
+    String cardHolder;
+    int balance;
+    String cardNumber;
+    PaymentSystem paymentSystem;
+
+}
---
+++ src/main/java/guru/qa/data/Main.java
@@ -0,0 +1,7 @@
+package guru.qa.data;
+
+public class Main {
+    public static void main(String[] args) {
+        Card masterCard = new Card();
+    }
+}
